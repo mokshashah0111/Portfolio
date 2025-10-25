67eaa4f9f59e837054f6a0308820bdbd311d1ea7
Moksha Shah <mokshashah1112@gmail.com>
2025-10-24 19:36:43 -0700

    Modify deploy and predeploy commands

--- src/data/site.js
+++ src/data/site.js
@@ -7,5 +7,6 @@ export const SITE = {
   phone: "",
   linkedin: "https://www.linkedin.com/in/your-handle",
   github: "https://github.com/mokshashah0111",
-  resumeUrl: "#"
+  rresumeUrl: `${import.meta.env.BASE_URL}Resume.pdf`,
+
 }
